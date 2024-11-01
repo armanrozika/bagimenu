5176690c8f137bd839736a998088b29d57d752e8
armanrozika <armanrozika@gmail.com>
2024-11-01 21:05:44 +0800

    change clerk to prod

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://loving-tadpole-58.clerk.accounts.dev",
+      domain: "https://clerk.bagimenu.com",
       applicationID: "convex",
     },
   ],
