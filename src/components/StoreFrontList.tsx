6b34ff3a547ecac5fc219622c0ac3c52e77e7607
armanrozika <armanrozika@gmail.com>
2024-11-02 07:33:55 +0800

    pending feature invoice

--- src/components/StoreFrontList.tsx
+++ src/components/StoreFrontList.tsx
@@ -1,10 +1,11 @@
-import { useMutation } from "convex/react";
+// import { useMutation } from "convex/react";
 import { FaWhatsapp } from "react-icons/fa";
-import { api } from "../../convex/_generated/api";
+// import { api } from "../../convex/_generated/api";
 import { ProductType, StoreType } from "../types/types";
 import { FiZoomIn } from "react-icons/fi";
 import Modal from "./Modal";
 import { useState } from "react";
+import { Link } from "@tanstack/react-router";
 
 type PropsType = {
   products: ProductType[];
@@ -12,34 +13,34 @@ type PropsType = {
 };
 
 function StoreFrontList({ products, store }: PropsType) {
-  const createInvoice = useMutation(api.invoices.add);
+  // const createInvoice = useMutation(api.invoices.add);
   const [showModal, setShowModal] = useState(false);
   const [activeProduct, setActiveProduct] = useState<ProductType | undefined>();
 
-  const generateInvoiceId = () => {
-    const st = store.url.toUpperCase().slice(0, 3);
-    const dt = new Date().toLocaleDateString().replaceAll("/", "");
-    const hh = new Date().getHours();
-    const mm = new Date().getMinutes();
-    const ss = new Date().getSeconds();
+  // const generateInvoiceId = () => {
+  //   const st = store.url.toUpperCase().slice(0, 3);
+  //   const dt = new Date().toLocaleDateString().replaceAll("/", "");
+  //   const hh = new Date().getHours();
+  //   const mm = new Date().getMinutes();
+  //   const ss = new Date().getSeconds();
 
-    const inv = `${st}-${dt}-${hh}${mm}${ss}`;
+  //   const inv = `${st}-${dt}-${hh}${mm}${ss}`;
 
-    return inv;
-  };
+  //   return inv;
+  // };
 
-  const sendInvoice = async (product_name: string, product_price: number) => {
-    const popup = window.open("");
-    const generatedId = generateInvoiceId();
-    await createInvoice({
-      invoice_id: generatedId,
-      product_name,
-      product_price,
-      is_success: false,
-      store_id: store._id,
-    });
-    popup!.location.href = `https://wa.me/+62${store.whatsapp}/?text=Halo kak, saya mau pesan ${product_name}, dengan harga ${formatPrice(product_price)} Terima kasih. [Id: ${generatedId}].`;
-  };
+  // const sendInvoice = async (product_name: string, product_price: number) => {
+  //   const popup = window.open("");
+  //   const generatedId = generateInvoiceId();
+  //   await createInvoice({
+  //     invoice_id: generatedId,
+  //     product_name,
+  //     product_price,
+  //     is_success: false,
+  //     store_id: store._id,
+  //   });
+  //   popup!.location.href = `https://wa.me/+62${store.whatsapp}/?text=Halo kak, saya mau pesan ${product_name}, dengan harga ${formatPrice(product_price)} Terima kasih. [Id: ${generatedId}].`;
+  // };
 
   const formatPrice = (price: number) => {
     return new Intl.NumberFormat("id-ID", {
@@ -89,20 +90,21 @@ function StoreFrontList({ products, store }: PropsType) {
                 maximumFractionDigits: 0,
               }).format(product.price)}
             </p>
-            {/* <Link
+            <Link
               target="_blank"
-              to={`https://wa.me/+62${store.whatsapp}/?text=Halo kak, saya mau pesan ${product.name}: ${product.notes}, dengan harga ${formatPrice(product.price)} Terima kasih`}
-              className="px-6 bg-amber-500 text-white rounded py-2 shadow-md"
+              to={`https://wa.me/+62${store.whatsapp}/?text=Halo kak, saya mau pesan ${product.name}, dengan harga ${formatPrice(product.price)} Terima kasih`}
+              className="bg-[#fa931c] w-full py-2 mt-2 rounded-lg text-white text-sm font-semibold flex justify-center items-center"
             >
-              Pesan via WhatsApp
-            </Link> */}
-            <button
+              Pesan
+              <FaWhatsapp className="ml-2 text-lg font-bold" />
+            </Link>
+            {/* <button
               className="bg-[#fa931c] w-full py-2 mt-2 rounded-lg text-white text-sm font-semibold flex justify-center items-center"
               onClick={() => sendInvoice(product.name, product.price)}
             >
               Pesan
               <FaWhatsapp className="ml-2 text-lg font-bold" />
-            </button>
+            </button> */}
           </div>
         </div>
       );
